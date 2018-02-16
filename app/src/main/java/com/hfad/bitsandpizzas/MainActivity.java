5b2ecc8e2a46d7b4557c95cb5504f089833453b0
vibhavk <vibhavkunj@hotmail.com>
2018-02-16 22:37:32 +0530

    v2.0 commit

--- app/src/main/java/com/hfad/bitsandpizzas/MainActivity.java
+++ app/src/main/java/com/hfad/bitsandpizzas/MainActivity.java
@@ -8,11 +8,16 @@ import android.view.MenuItem;
 import android.content.Intent;
 import android.support.v7.widget.ShareActionProvider;
 import android.support.v4.view.MenuItemCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.design.widget.TabLayout;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private  ShareActionProvider shareActionProvider;
+    private ShareActionProvider shareActionProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +25,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        SectionsPagerAdapter pagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        ViewPager pager = (ViewPager) findViewById(R.id.pager);
+        pager.setAdapter(pagerAdapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(pager);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu){
+    public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         MenuItem menuItem = menu.findItem(R.id.action_share);
         shareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
@@ -32,12 +45,52 @@ public class MainActivity extends AppCompatActivity {
         return super.onCreateOptionsMenu(menu);
     }
 
-    private void setShareActionIntent(String text){
+    private void setShareActionIntent(String text) {
         Intent intent = new Intent(Intent.ACTION_SEND);
         intent.setType("text/plain");
         intent.putExtra(Intent.EXTRA_TEXT, text);
         shareActionProvider.setShareIntent(intent);
     }
+
+    private class SectionsPagerAdapter extends FragmentPagerAdapter {
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public int getCount() {
+            return 4;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return new TopFragment();
+                case 1:
+                    return new PizzaFragment();
+                case 2:
+                    return new PastaFragment();
+                case 3:
+                    return new StoresFragment();
+            }
+
+            return null;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position){
+            switch (position){
+                case 0: return getResources().getText(R.string.home_tab);
+                case 1: return getResources().getText(R.string.pizza_tab);
+                case 2: return getResources().getText(R.string.pasta_tab);
+                case 3: return getResources().getText(R.string.store_tab);
+            }
+
+            return null;
+        }
+    }
+
 
         @Override
              public boolean onOptionsItemSelected(MenuItem item){
--- app/src/main/java/com/hfad/bitsandpizzas/OrderActivity.java
+++ app/src/main/java/com/hfad/bitsandpizzas/OrderActivity.java
@@ -1,9 +1,12 @@
 package com.hfad.bitsandpizzas;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
 
 public class OrderActivity extends AppCompatActivity {
 
@@ -13,8 +16,23 @@ public class OrderActivity extends AppCompatActivity {
         setContentView(R.layout.activity_order);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        // enable up button
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    public void onClickDone(View view) {
+        CharSequence text = "This is a FA Button, order updated";
+        int duration = Snackbar.LENGTH_INDEFINITE;
+        Snackbar snackbar = Snackbar.make(findViewById(R.id.coordinator), text, duration);
+        snackbar.setAction("Undo?",
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast toast = Toast.makeText(OrderActivity.this, "Undone!",
+                                Toast.LENGTH_SHORT);
+                        toast.show();
+                    }
+                });
+        snackbar.show();
     }
 }
